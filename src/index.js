6a330187df029ef9ce462542cff154125f83cee9
MrSayyod <ssayyod@gmail.com>
2021-04-17 07:16:34 -0500

    Extract necessary data and show it in console

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 const  apiKey = "1ebf85132fae8866f42ade66510b07df"
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=Tashkent&appid=${apiKey}`,
-{ mode: 'cors'})
-.then(function(response) {
-  return response.json()
-})
-.then(function(response) {
-  console.log(response)
-})
-.catch(function(error) {
-  console.log(error)
+const search = document.getElementById('search')
+const submit = document.getElementById("submit")
+
+function getResult() {
+  const city = search.value
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`,
+  { mode: 'cors'})
+  .then(function(response) {
+    return response.json()
+  })
+  .then(function(response) {
+    // console.log(response);
+    let result = {
+      "city": response.name,
+      "temperature" : response.main.temp 
+    }
+    console.log(`City: ${result.city} \n Temperate: ${result.temperature} in Kalvin`)
+    let jsonResult =  JSON.stringify(result)
+    console.log(jsonResult);
+    return jsonResult
+  })
+  .catch(function(error) {
+    console.log(error)
+    console.log("City not found")
+  })
+}
+
+submit.addEventListener('click', (e)=> {
+  e.preventDefault()
+  getResult()
 })
 
 
