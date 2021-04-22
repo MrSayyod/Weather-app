ef83a2ca356f2da158e658e9956b8933bd6ecd3c
MrSayyod <ssayyod@gmail.com>
2021-04-23 02:58:38 +0500

    Delete unnecessary codes and comments

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-// import getResult from './workingWithAPI';
 import { nodeCreator, chainAppend } from './nodeCreator'
 
 const  apiKey = "1ebf85132fae8866f42ade66510b07df"
 const search = document.getElementById('search')
 const submit = document.getElementById("submit")
-const display = document.getElementById('display')
-  
-  function getResult() {
-  const city = document.getElementById('city')
-  const weather = document.getElementById('weather')
-  const weatherTypeText = document.getElementById('for_text')
-  const weatherTypeImage = document.getElementById('for_img')
+function getResult() {
+  const body = document.querySelector('body')
+  const display = nodeCreator('div', {id: 'display'})
+  const city = nodeCreator('div', {id: 'city'})
+  const weather = nodeCreator('div', {id: 'weather'})
+  const weatherType = nodeCreator('div', {id: 'weather_type'})
+  const weatherTypeText = nodeCreator('span', { id: 'for_text'})
   const cityName = search.value
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`,
   { mode: 'cors'})
@@ -18,23 +17,13 @@ const display = document.getElementById('display')
     return response.json()
   })
   .then(function(response) {
-    // console.log(response);
-    let result = {
-      "city": response.name,
-      "temperature" : response.main.temp 
-    }
-
+    chainAppend([display, city])
+    chainAppend([display, weather])
+    chainAppend([display, weatherType, weatherTypeText])
+    chainAppend([body, display])
     city.textContent = response.name
     weather.textContent = response.main.temp - 273.15 + "° C"
     weatherTypeText.textContent = response.weather[0].main
-    // console.log(response.weather);
-    // chainAppend(display, city)
-    console.log(`City: ${result.city} \n Temperate: ${result.temperature} in Kalvin`)
-    let jsonResult =  JSON.stringify(response)
-    console.log(jsonResult);
-    console.log(response);
-    // console.log(result);
-    return jsonResult
   })
   .catch(function(error) {
     console.log(error)
@@ -46,18 +35,3 @@ submit.addEventListener('click', (e)=> {
   e.preventDefault()
   getResult()
 })
-
-// const body = document.querySelector('body')
-//  const main = document.createElement('main')
-//  const container = document.createElement('section')
-//  container.setAttribute('class', 'container')
-//  const div = document.createElement('div')
- 
-// let nodes = [body, main, container, div]
-// for (let i = nodes.length-2; i>=0; i--) {
-// 	nodes[i].appendChild(nodes[i+1])
-// }
-
-
-
-// `http://api.openweathermap.org/data/2.5/weather?q=Tashkent&appid=${apiKey}`
