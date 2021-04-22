fd9dadd7873a0f413b85101602af112b515d2454
MrSayyod <ssayyod@gmail.com>
2021-04-23 04:39:34 +0500

    Add linter files and fix errors

--- src/index.js
+++ src/index.js
@@ -5,37 +5,37 @@ const search = document.getElementById('search');
 const submit = document.getElementById('submit');
 function getResult() {
   const body = document.querySelector('body');
-  const display = document.getElementById('display')
+  const display = document.getElementById('display');
   const city = nodeCreator('div', { id: 'city' });
   const weather = nodeCreator('div', { id: 'weather' });
   const weatherType = nodeCreator('div', { id: 'weather_type' });
   const weatherTypeText = nodeCreator('span', { id: 'for_text' });
   const err = nodeCreator('div', { id: 'error' });
   const cityName = search.value;
-  let degree = null
+  let degree = null;
   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`,
     { mode: 'cors' })
     .then((response) => response.json())
     .then((response) => {
       if (display.firstChild) {
         while (display.firstChild) {
-            display.removeChild(display.firstChild);
+          display.removeChild(display.firstChild);
         }
-    }
+      }
       chainAppend([display, city]);
       chainAppend([display, weather]);
       chainAppend([display, weatherType, weatherTypeText]);
       city.textContent = response.name;
-      degree = (response.main.temp - 273.15).toFixed(1)
+      degree = (response.main.temp - 273.15).toFixed(1);
       weather.textContent = `${degree}° C`;
       weatherTypeText.textContent = response.weather[0].main;
     })
     .catch(() => {
       if (display.firstChild) {
         while (display.firstChild) {
-            display.removeChild(display.firstChild);
+          display.removeChild(display.firstChild);
         }
-    }
+      }
       err.textContent = 'City not found';
       chainAppend([body, display, err]);
     });
