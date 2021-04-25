002dbb8388006e50f80b31cf585df2f72541b6ce
MrSayyod <ssayyod@gmail.com>
2021-04-25 05:49:32 -0600

    Add toggle function to see results  in Celcius or Fahrenheit

--- src/index.js
+++ src/index.js
@@ -3,18 +3,40 @@ import { nodeCreator, chainAppend } from './nodeCreator';
 const apiKey = '1ebf85132fae8866f42ade66510b07df';
 const search = document.getElementById('search');
 const submit = document.getElementById('submit');
-const toggler = document.getElementById('toggler')
 
-function displayToggler(display) {
-  const toggler = nodeCreator('button', {id: "toggler", class: "btn btn-primary"}, "℃ / ℉")
+function displayToggler(display, number, text) {
+  const toggler = nodeCreator('button', { id: "toggler", class: "btn btn-primary" }, "°C / °F")
+  toggler.addEventListener('click', ()=> {
+    let degree = Number(number.textContent)
+    if (text.textContent == '°C') {
+      degree = ((degree * 9 / 5) + 32).toFixed(1)
+      number.textContent = degree    
+      text.textContent = '°F'
+    } else if (text.textContent == '°F') {
+      degree = ((degree - 32) * 5 / 9).toFixed(1)
+      number.textContent = degree   
+     text.textContent = '°C'
+    }
+  })
+
   chainAppend([display, toggler])
+}
+
+function removeChild(parent) {
+  if (parent.firstChild) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
 }
 
 function getResult() {
   const body = document.querySelector('body');
   const display = document.getElementById('display');
   const city = nodeCreator('div', { id: 'city' });
-  const weather = nodeCreator('div', { id: 'weather' });
+  const weather = nodeCreator('div', { id: 'weather', class: "celcius" });
+  const degreeNumber = nodeCreator('span', { id: "degree_number" })
+  const degreeText = nodeCreator('span', { id: "degree_text" }, "°C")
   const weatherType = nodeCreator('div', { id: 'weather_type' });
   const weatherTypeText = nodeCreator('span', { id: 'for_text' });
   const err = nodeCreator('div', { id: 'error' });
@@ -24,27 +46,20 @@ function getResult() {
     { mode: 'cors' })
     .then((response) => response.json())
     .then((response) => {
-      if (display.firstChild) {
-        while (display.firstChild) {
-          display.removeChild(display.firstChild);
-        }
-      }
-    
+      removeChild(display)
       city.textContent = response.name;
       degree = (response.main.temp - 273.15).toFixed(1);
-      weather.textContent = `${degree}° C`;
+      degreeNumber.textContent = `${degree}`;
       weatherTypeText.textContent = response.weather[0].main;
       chainAppend([display, city]);
+      chainAppend([weather, degreeNumber]);
+      chainAppend([weather, degreeText]);
       chainAppend([display, weather]);
       chainAppend([display, weatherType, weatherTypeText]);
-      displayToggler(display)
+      displayToggler(display, degreeNumber, degreeText)
     })
     .catch(() => {
-      if (display.firstChild) {
-        while (display.firstChild) {
-          display.removeChild(display.firstChild);
-        }
-      }
+      removeChild(display)
       err.textContent = 'City not found';
       chainAppend([body, display, err]);
     });
